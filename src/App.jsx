46b241d2aea3439283c1cb52709092789d9652ec
kkyusuftk <yusuf@clevertap.com>
2024-05-29 23:40:28 +0530

    feat: removed api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-import { useEffect } from "react";
-import { getRequestWithHeaders } from "./api";
 import styles from "./app.module.css";
 
 function App() {
-	useEffect(() => {
-		getRequestWithHeaders(
-			"https://api.themoviedb.org/3/authentication/token/new",
-		)
-			.then((value) => value.json())
-			.then((data) => console.log(data));
-	}, []);
 	return <div className={styles.container}>Pitchure - The movies app</div>;
 }
 
